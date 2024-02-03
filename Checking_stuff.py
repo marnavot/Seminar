8ea3b4afbae8c216e613277aee70f581a758a6a2
marnavot <158297679+marnavot@users.noreply.github.com>
2024-02-03 11:57:38 +0200

    Add files via upload

--- Checking_stuff.py
+++ Checking_stuff.py
@@ -4,8 +4,11 @@ from gensim.test.utils import datapath
 from scipy.stats import spearmanr, pearsonr
 import matplotlib.pyplot as plt
 
-folder = "/cs/labs/oabend/tomer.navot/decade_models/0/"
-for filename in os.listdir(folder):
-    model = Word2Vec.load(folder + filename)
-    print(f"loaded model {filename}")
-    print(model.wv.most_similar("man"))
+folder = "/cs/labs/oabend/tomer.navot/decade_models/"
+for i in range(10):
+    sub_folder = f"{folder}{i}/"
+        for filename in os.listdir(sub_folder):
+            print(f"loading {filename}")
+            model = Word2Vec.load(sub_folder + filename)
+            print(f"loaded model {filename}")
+            print(model.wv.most_similar("man"))
