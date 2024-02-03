b406ed6610e08077aa78c6a3852845b1ebb74a8d
marnavot <158297679+marnavot@users.noreply.github.com>
2024-02-03 12:12:52 +0200

    Add files via upload

--- Checking_stuff.py
+++ Checking_stuff.py
@@ -5,10 +5,11 @@ from scipy.stats import spearmanr, pearsonr
 import matplotlib.pyplot as plt
 
 folder = "/cs/labs/oabend/tomer.navot/decade_models/"
-for i in range(10):
+for i in range(10)
     sub_folder = f"{folder}{i}/"
     for filename in os.listdir(sub_folder):
-        print(f"loading {filename}")
-        model = Word2Vec.load(sub_folder + filename)
-        print(f"loaded model {filename}")
-        print(model.wv.most_similar("man"))
+        if filename.endswith(".model"):
+            print(f"loading {filename}")
+            model = Word2Vec.load(sub_folder + filename)
+            print(f"loaded model {filename}")
+            print(model.wv.most_similar("man"))
